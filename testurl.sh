2527c61952d6abbdee2a1ad3ca82bf4f131785ac
yuanyulee <yujioda@gmail.com>
2016-05-31 22:31:46 +0800

    Add Test http Shell Script

---
+++ testurl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+test="$(/usr/bin/curl -I -s $1 | grep 'HTTP/1.1 200')"
+while true; do
+  datestr="$(/bin/date +%Y%m%d-%H:%M:%S)"
+  if [ "$test" == "" ] ; then
+    echo "$1;0;$datestr" >> log.txt
+    echo "$1;0;$datestr"
+  else
+    echo "$1;1;$datestr" >> log.txt
+    echo "$1;1;$datestr"
+  fi
+  sleep 2s
+done
